897fc5db78f7576e9abc5a047773602d6a3cc3b5
4nc3str4l <cmuri_10@hotmail.com>
2020-04-05 12:51:40 +0200

    Loading static

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+	"path/filepath"
+)
+
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	// Load the HTML Folders
+	htmlPath := filepath.FromSlash("./templates/**.html")
+	r.LoadHTMLGlob(htmlPath)
+
+	// Load the static files
+	cssPath := filepath.FromSlash("./static/css")
+	r.Static("/css", cssPath)
+	
+	r.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
+	
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
